03101dd3a2fdefe993f8a6ac7dea78a2b407b913
Ben Eggett <beneggett@gmail.com>
2012-05-14 03:30:56 -0600

    changed hook to link to bottom of description box

--- app/overrides/add_email_to_friend_link_to_products.rb
+++ app/overrides/add_email_to_friend_link_to_products.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "converted_product_description_351026984",
-                     :insert_bottom => "[data-hook='product_description'], #product_description[data-hook]",
+                     :insert_bottom => "[data-hook='description']",
                      :text => "<p class=\"email_to_friend\">
-        <%= link_to t('email_to_friend.send_to_friend'), email_to_friend_url('product', @product) %>
+        <%= link_to t('email_to_friend.send_to_friend'), email_to_friend_url('product', @product), :class => 'button' %>
     </p>",
                      :disabled => false)
